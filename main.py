28ef77e6f058a4b3dc0a9f2815ffdfe64cbb8821
VidalGB <96237463+VidalGB@users.noreply.github.com>
2022-02-20 08:21:41 -0300

    incorporación de nuevas pantallas y funciones, también se ramifico en carpetas

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ Window.size = (840, 470)
 
 #	funciones de letura y escritura
 def lectura(variable):
-	with open ('datos.csv') as archivo:
+	with open ('datos/datos.csv') as archivo:
 		contenido = csv.reader(archivo, delimiter = ';')
 		for line in contenido:
 			if line[0] == variable:
@@ -34,7 +34,7 @@ def lectura(variable):
 def escritura(variable, escribir):
 	guardado = []
 	n = 0
-	with open ('datos.csv') as archivo:
+	with open ('datos/datos.csv') as archivo:
 		contenido = csv.reader(archivo, delimiter = ';')
 		for line in contenido:
 			if line[0] == variable:
@@ -45,7 +45,7 @@ def escritura(variable, escribir):
 			if elemento == []:
 				del guardado[n]
 			n += 1
-	with open ('datos.csv', 'w', newline = '') as archivo:
+	with open ('datos/datos.csv', 'w', newline = '') as archivo:
 		write = csv.writer(archivo, delimiter = ';')
 		write.writerows(guardado)
 		write.writerow([variable,escribir])
@@ -53,7 +53,7 @@ def escritura(variable, escribir):
 
 def lectura2(ID):
 	idiomas = {"Español":1,"English":2, "Portugues":3}
-	with open ('textos.csv') as archivo:
+	with open ('datos/textos.csv') as archivo:
 		contenido = csv.reader(archivo, delimiter = ';')
 		posicion = idiomas[lectura("lenguage")]
 		for line in contenido:
@@ -88,6 +88,7 @@ class Internet(MDApp):
 	inicio = lectura("inicio")
 	dialog_oscuro = None
 	dialog_idioma = None
+	ERROR = None
 	lenguaje = lectura("lenguage")
 	global idiomas
 	idiomas = {"Español":1,"English":2, "Portugues":3}
@@ -98,9 +99,10 @@ class Internet(MDApp):
 #	funciones de inicio
 	def __init__(self, *args, **kwargs):
 		MDApp.__init__(self, *args, **kwargs)
-		self.title = "InternetSpeed"
+		self.title = "QuickWify"
 		self.theme_cls.primary_palette = "Blue"
 		self.theme_cls.accent_palette = "Teal"
+		self.icon="datos/imagenes/logo.ico"
 		self.theme_cls.theme_style = lectura("color")
 
 	def on_start(self):
@@ -111,13 +113,14 @@ class Internet(MDApp):
 
 #	contruccion
 	def build(self):
-		Screen_M.add_widget(Builder.load_file("logo_screen.kv"))
-		Screen_M.add_widget(Builder.load_file("inicio_screen.kv"))
-		Screen_M.add_widget(Builder.load_file("login_screen.kv"))
-		Screen_M.add_widget(Builder.load_file("info_screen.kv"))
-		Screen_M.add_widget(Builder.load_file("settings_screen.kv"))
-		Screen_M.add_widget(Builder.load_file("resultados_screen.kv"))
-		Screen_M.add_widget(Builder.load_file("datos_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/logo_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/inicio_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/login_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/info_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/settings_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/resultados_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/datos_screen.kv"))
+		Screen_M.add_widget(Builder.load_file("datos/screen/error_screen.kv"))
 		escritura("progreso", "0")
 		menu_items = [{"viewclass": "OneLineListItem", "text": f"{self.mostrar('b_ajustes', True)}", "height": 40, "on_release": lambda x=f"{self.mostrar('b_ajustes', True)}": self.cambio_screen("settings_screen")},{"viewclass": "OneLineListItem","text": f"{self.mostrar('b_info', True)}","height": 45,"on_release": lambda x=f"{self.mostrar('b_info', True)}": self.cambio_screen("info_screen")}]
 		self.menu = MDDropdownMenu(items=menu_items,width_mult=2.3,)
@@ -145,6 +148,8 @@ class Internet(MDApp):
 			self.cambio_screen("inicio_screen")
 			self.settings_actualizar()
 			self.datos_actualizar()
+			self.info_actualizar()
+			self.error_actualizar()
 
 #	modo oscuro
 	def modo_oscuro_dialog(self):
@@ -167,25 +172,25 @@ class Internet(MDApp):
 		self.dialog_oscuro.dismiss()
 		self.dialog_oscuro = MDDialog(text = f"{self.mostrar('modo_oscuro', True)}",radius=[20, 7, 20, 7],buttons =[MDFlatButton(text = f"{self.mostrar('b2_modo_oscuro', True)}", theme_text_color = "Custom", text_color = self.theme_cls.primary_color, on_release = self.modo_oscuro_off),],)
 
+#	test wifi
+	def Test(self):
+		try:
+			wifi = speedtest.Speedtest()
+			time = datetime.datetime.now().strftime("%D · %H:%M")
+			escritura("fecha", time)
+			download = round((round(wifi.download()) / 1048576), 2)
+			escritura("descarga", download)
+			upload = round((round(wifi.upload()) / 1048576), 2)
+			escritura("subida", upload)
+			ping = wifi.results.ping
+			escritura("ping", ping)
+		except:
+			return True
+
 #	menu
 	def abrir(self, button):
 		self.menu.caller = button
 		self.menu.open()
-
-#	test wifi
-	def Test(self):
-		wifi = speedtest.Speedtest()
-		servernames = []
-		wifi.get_servers(servernames)
-		time = datetime.datetime.now().strftime("%D · %H:%M")
-		escritura("fecha", time)
-		download = round((round(wifi.download()) / 1048576), 2)
-		escritura("descarga", download)
-		upload = round((round(wifi.upload()) / 1048576), 2)
-		escritura("subida", upload)
-		ping = wifi.results.ping
-		escritura("ping", ping)
-		print(f"servernames{servernames}")
 
 #	cambio de pantalla
 
@@ -233,7 +238,7 @@ class Internet(MDApp):
 #	funciones de muestreo
 	def mostrar(self, ID, clasificar):
 		if clasificar:
-			with open ('textos.csv') as archivo:
+			with open ('datos/textos.csv') as archivo:
 				contenido = csv.reader(archivo, delimiter = ';')
 				posicion = idiomas[self.lenguaje]
 				for line in contenido:
@@ -243,26 +248,10 @@ class Internet(MDApp):
 			prin = lectura(f"{ID}")
 			return prin
 
-	def señal(self, ID):
-		d_subida = int(lectura("datos_subida"))
-		d_descarga = int(lectura("datos_descarga"))
-		subida = round(float(lectura("subida")))
-		descarga = round(float(lectura("descarga")))
-		por_download = descarga * 100 / d_descarga
-		por_upload = subida * 100 / d_subida
-		señal = por_upload + por_download / 2
-		if señal >= 50:
-			entrada = 0
-		else:
-			entrada = 1
-		with open ('textos.csv') as archivo:
-			contenido = csv.reader(archivo, delimiter = ';')
-			posicion = idiomas[self.lenguaje]
-			for line in contenido:
-				if line[0] == ID:
-					pos = line[posicion]
-					Pos = pos.split(sep=",")
-					return Pos[entrada]
+	def text_info(self):
+		List = ["  QuickWify es una aplicación de escritorio creada con Python, \n  para poder medir las velocidades tanto de subida como de bajada de su internet.\n  QuickWify no guarda ni recopila ningún dato sobre sus usuarios.\n  Esta aplicación fue creada sin fines de lucro por @VidalGB. \n  Otros agradecimientos a los traductores y a la diseñadora/artista @rxaxven (Instagram)","  QuickWify is a desktop application created with Python,\n  to measure both the upload and download speed of your internet.\n  QuickWify does not save or collect any data about its users.\n  This application was created for non-profit by @VidalGB.\n  Other thanks to the translators and the designer/artist @rxaxven (Instagram)","  QuickWify é um aplicativo de desktop criado com Python,\n  para poder medir as velocidades de upload e download da sua internet.\n  QuickWify não salva nem coleta dados sobre seus usuários.\n  Este aplicativo foi criado sem fins lucrativos por @VidalGB.\n  Outros agradecimentos aos tradutores e ao designer/artista @rxaxven (Instagram)"]
+		posicion = idiomas[self.lenguaje] -1
+		return List[posicion]
 
 	def login_actualizar(self):
 		Screen_M.get_screen('login_screen').ids.Bar.title = f"{self.mostrar('p_medicion', True)}"
@@ -276,8 +265,15 @@ class Internet(MDApp):
 				text_porsentaje.text = f"{self.mostrar('progreso', True)}: {self.mostrar('progreso', False)}%"
 				value_bar = Screen_M.get_screen('login_screen').ids.bar
 				value_bar.value = int(lectura('progreso'))
-				#if n > 75:
-					#self.Test()
+				if n > 75:
+					self.ERROR = self.Test()
+				if self.ERROR:
+					escritura("progreso", "0")
+					self.cambio_screen("error_screen")
+					value_bar.value = 0
+					text_porsentaje.text = f"{self.mostrar('progreso', True)}: {self.mostrar('progreso', False)}%"
+					self.ERROR = False
+					break
 				if n == 100:
 					escritura("progreso", "0")
 					await asynckivy.sleep(1)
@@ -294,12 +290,29 @@ class Internet(MDApp):
 		descarga = round(float(lectura("descarga")))
 		por_download = descarga * 100 / d_descarga
 		por_upload = subida * 100 / d_subida
+		if por_download <= 33 and por_download >= 0:
+			Screen_M.get_screen('resultados_screen').ids.download.color = (1,0,0,1)
+			Screen_M.get_screen('resultados_screen').ids.t_download.text_color = (1,0,0,1)
+		elif por_download <= 66 and por_download > 33:
+			Screen_M.get_screen('resultados_screen').ids.download.color = (0,0,1,1)
+			Screen_M.get_screen('resultados_screen').ids.t_download.text_color = (0,0,1,1)
+		elif por_download > 66:
+			Screen_M.get_screen('resultados_screen').ids.download.color = (0,1,0,1)
+			Screen_M.get_screen('resultados_screen').ids.t_download.text_color = (0,1,0,1)
+		if por_upload <= 33 and por_upload >= 0:
+			Screen_M.get_screen('resultados_screen').ids.upload.color = (1,0,0,1)
+			Screen_M.get_screen('resultados_screen').ids.t_upload.text_color = (1,0,0,1)
+		elif por_upload <= 66 and por_upload > 33:
+			Screen_M.get_screen('resultados_screen').ids.upload.color = (0,0,1,1)
+			Screen_M.get_screen('resultados_screen').ids.t_upload.text_color = (0,0,1,1)
+		elif por_upload > 66:
+			Screen_M.get_screen('resultados_screen').ids.upload.color = (0,1,0,1)
+			Screen_M.get_screen('resultados_screen').ids.t_upload.text_color = (0,1,0,1)
 		Screen_M.get_screen('resultados_screen').ids.upload.value = por_upload
 		Screen_M.get_screen('resultados_screen').ids.download.value = por_download
 		Screen_M.get_screen('resultados_screen').ids.t_upload.text = f"{self.mostrar('subida', False)} MB/s"
 		Screen_M.get_screen('resultados_screen').ids.t_download.text = f"{self.mostrar('descarga', False)} MB/s"
 		Screen_M.get_screen('resultados_screen').ids.t_ping.text = f"{self.mostrar('ping', False)} Ping"
-		Screen_M.get_screen('resultados_screen').ids.señal.text = f"{self.señal('señal')}"
 
 	def inicio_actualizar(self):
 		Screen_M.get_screen('inicio_screen').ids.t_tiempo.text = f"{self.mostrar('tiempo', True)}: {self.mostrar('fecha', False)}"
@@ -344,10 +357,21 @@ class Internet(MDApp):
 		Screen_M.get_screen('datos_screen').ids.bajada.helper_text = f"{self.mostrar('cuadro_b-a', True)}"
 		Screen_M.get_screen('datos_screen').ids.button.text = f"{self.mostrar('B_conf', True)}"
 
-#	Botones
-	def hola(self):
-		webbrowser.open("https://github.com/VidalGB/InternetPy", new=2, autoraise=True)
+	def info_actualizar(self):
+		Screen_M.get_screen('info_screen').ids.bar.title = f"{self.mostrar('b_info', True)}"
+		Screen_M.get_screen('info_screen').ids.text.text = self.text_info()
+		Screen_M.get_screen('info_screen').ids.codigo.text = f"{self.mostrar('b_codigo', True)}"
+		Screen_M.get_screen('info_screen').ids.contacto.text = f"{self.mostrar('b_contacto', True)}"
 
+	def error_actualizar(self):
+		Screen_M.get_screen('error_screen').ids.t_error.text = f"{self.mostrar('t_error', True)}"
+
+#	Botones Info
+	def button_info(self, contacto):
+		if not contacto:
+			webbrowser.open("https://github.com/VidalGB/InternetPy", new=2, autoraise=True)
+		else:
+			webbrowser.open("https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcSHvnnrkvHzbwwdSPWmlFnjcKtRqFsQLTlCCVVWDMwmLbnjKcGhmHfTWScpnMGZVjvBxhVSD", new=2, autoraise=True)
 
 #	inicio
 if __name__ == '__main__':
